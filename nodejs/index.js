2d987eb53048292258d97b2e33acee3a32e84bc2
Karikumutima Honore <141430776+karikumutimahonore@users.noreply.github.com>
2024-09-04 08:44:43 +0200

    Node Crash Course

---
+++ nodejs/index.js
@@ -0,0 +1,11 @@
+import getposts, { getpostsLength } from "./postController.js";
+
+console.log(getposts());
+
+console.log(`Posts Length: ${getpostsLength()}`);
+
+// const { generateRandomNmber, celciusToFahrenheit } = require("./utilis");
+
+// console.log(`Random Number: ${generateRandomNmber()}`);
+
+// console.log(`Celcius to Fahrinheit: ${celciusToFahrenheit(10)}`);
---
+++ nodejs/postController.js
@@ -0,0 +1,10 @@
+const posts = [
+  { id: 1, title: "post one" },
+  { id: 2, title: "post two" },
+];
+
+export const getposts = () => posts;
+
+export const getpostsLength = () => posts.length;
+
+export default getposts;
